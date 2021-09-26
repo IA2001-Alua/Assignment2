ffa33e9c704fd3145b91a36aca1d81da3909d06d
aluazhexenbi <>
2021-09-26 10:56:49 +0000

    Initial commit

---
+++ app.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include "main.h"
+using namespace std;
+
+void changeLuck(Person &person1, Person &p2) {
+    int luck = person1.getLuck();
+    person1.setLuck(p2.getLuck());
+    p2.setLuck(luck);
+}
+
+int main()
+{
+    srand(time(0));
+    Person* a = new Person("Ann", 24, rand() % 100);
+    Person* b = new Person("Blake", 33, rand() % 100);
+    Person* c = new Person("Bob", 7, rand() % 100);
+    Person* d = new Person("Ryan", 44, rand() % 100);
+    Person* e = new Person("Ari", 20, rand() % 100);
+    vector<Person> Persons { *a, *b, *c, *d, *e };
+
+    for (int i = 0; i < 5; i++) {
+        cout << Persons[i].getName() << " " << Persons[i].getAge()<< " " << Persons[i].getLuck()<< endl;
+    }
+
+    
+
+    delete a, b, c, d, e;
+
+}
---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "main.h"
+
+
+
+Person::Person(string name, int age, int luck)
+{
+	this->name = name;
+	this->age = age;
+	this->luck = luck;
+}
+
+Person::Person(){}
+
+void Person::setName(string name)
+{
+	this->name = name;
+}
+
+void Person::setAge(int age)
+{
+	this->age = age;
+}
+
+void Person::setLuck(int luck)
+{
+	this->luck = luck;
+}
+
+string Person::getName()
+{
+	return name;
+}
+
+int Person::getAge()
+{
+	return age;
+}
+
+int Person::getLuck()
+{
+	return luck;
+}
+
+string Person::getlLucky()
+{
+	string getlucky = " ";
+	cout << getlucky << endl;
+	return getlucky;
+}
