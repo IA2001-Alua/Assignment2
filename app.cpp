cc2d595876ce6979d3b60d41258867adbee91e14
aluazhexenbi <79184514+IA2001-Alua@users.noreply.github.com>
2021-09-26 11:35:43 +0000

    1

--- app.cpp
+++ app.cpp
@@ -5,10 +5,10 @@
 #include "main.h"
 using namespace std;
 
-void changeLuck(Person &person1, Person &p2) {
+void changeLuck(Person &person1, Person &person2) {
     int luck = person1.getLuck();
-    person1.setLuck(p2.getLuck());
-    p2.setLuck(luck);
+    person1.setLuck(person2.getLuck());
+    person2.setLuck(luck);
 }
 
 int main()
@@ -19,26 +19,6 @@ int main()
     Person* c = new Person("Bob", 7, rand() % 100);
     Person* d = new Person("Ryan", 44, rand() % 100);
     Person* e = new Person("Ari", 20, rand() % 100);
-    vector<Person> Persons { *a, *b, *c, *d, *e };
-
-    for (int i = 0; i < 5; i++) {
-        cout << Persons[i].getName() << " " << Persons[i].getAge()<< " " << Persons[i].getLuck()<< endl;
-    }
-    cout << "Luckiest person: ";
-    int max = Persons[0].getLuck();
-    int index;
-    for (int i = 0; i < 5; i++) {
-        if (max < Persons[i].getLuck()) {
-            max = Persons[i].getLuck();
-            index = i;
-        }
-    }
-    cout << Persons[index].getName()<< ", rate of luck "<< Persons[index].getLuck() << endl;
-
-    cout << "Luck of first person: " << Persons[0].getLuck() << ". Luck of second person: " << Persons[1].getLuck() << endl;
-    changeLuck(Persons[0], Persons[1]);
-    cout << "Luck of first person: " << Persons[0].getLuck() << ". Luck of second person: " << Persons[1].getLuck();
-
-    delete a, b, c, d, e;
+    vector<Person> People { *a, *b, *c, *d, *e };
 
 }
